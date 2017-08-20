64c17d83e7aefc5d86545101536996cb501b2780
117 <z351622948@126.com>
2017-08-20 21:51:02 +0800

    更新

--- appsys/src/cn/appsys/dao/dataDictionary/DataDictionaryMapper.java
+++
@@ -1,9 +0,0 @@
-package cn.appsys.dao.dataDictionary;
-
-import java.util.List;
-
-import cn.appsys.pojo.DataDictionary;
-
-public interface DataDictionaryMapper {
-	List<DataDictionary> getDataDictionaries(String typeCode);
-}
